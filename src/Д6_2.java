bad909a840f91dbbbd277cfc9301b4e2e0ec7000
zyza89 <zuza89@mail.ru>
2023-09-03 15:22:30 +0300

    Тест 2

--- src/Д6_2.java
+++ src/Д6_2.java
@@ -1,5 +1,14 @@
 public class Д6_2 {
     public static void main(String[] args){
-        
+//        Задача №1
+
+        int[] array = {0,1,2,3,4,5,6,7,8,9};
+        for (int i = 0; i < array.length; i++) {
+            for(int j = 0;j < array.length - j;  j++)
+            System.out.println(array[j] + "");
+        }
+        System.out.println();
+
+
     }
 }
